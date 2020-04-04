66eb638fb17c30b8af024961422c38ad50bad8d8
Frank Hui <huifrank@live.com>
2020-04-03 21:51:33 -0700

    Add semi functional item and raw ingredient rendering

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import json
 import datetime
 import time
-from flask import Flask, request, render_template, send_from_directory, session, flash, redirect
+from flask import Flask, request, render_template, send_from_directory, session, flash, redirect, Response
 from flask_wtf import FlaskForm
 from whoosh.query import Every
 from wtforms import SelectMultipleField, TextAreaField, SubmitField, StringField
@@ -17,11 +17,14 @@ from os import walk
 
 app = Flask(__name__)
 
+app.secret_key = 'Zli6WMDUEboJnp34fzwK'.encode('utf8')
+
 recipes = []
 icons = []
 
 schema = Schema(title=TEXT(stored=True), path=ID(stored=True), group=TEXT(stored=True), type=KEYWORD(stored=True),
-                icon=ID(stored=True), pattern=STORED, key=STORED, result=STORED, ingredients=STORED, count=NUMERIC(stored=True))
+                icon=ID(stored=True), pattern=STORED, key=STORED, result=STORED, ingredients=STORED,
+                count=NUMERIC(stored=True))
 if not os.path.exists("whoosh"):
     os.mkdir("whoosh")
 ix = create_in("whoosh", schema)
@@ -53,6 +56,11 @@ def load_icons():
         icons.extend(filenames)
         break
 
+
+def itemToRecipePath(item: str):
+    return item[item.find(":") + 1:] + ".json"
+
+
 def load_search_engine():
     writer = ix.writer()
     for recipe in recipes:
@@ -67,25 +75,114 @@ def load_search_engine():
             key = data['key'] if 'key' in data else {}
             result = data['result'] if 'result' in data else {}
             pattern = data['pattern'] if 'pattern' in data else []
-            ingredients = data['ingredients'] if 'ingredients' in data else (data['ingredient'] if 'ingredient' in data else "")
+            ingredients = data['ingredients'] if 'ingredients' in data else (
+                data['ingredient'] if 'ingredient' in data else "")
             count = data['count'] if 'count' in data else -1
         writer.add_document(title=" ".join([x.capitalize() for x in recipe.replace(".json", "").split("_")]),
                             path=recipe, pattern=pattern, key=key, result=result, group=group, type=type,
                             ingredients=ingredients, count=count, icon=icon)
     writer.commit()
 
+
 @app.route('/search', methods=['GET', 'POST'])
 def ajaxSearch():
     return json.dumps(search(request.args.get("term")))
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add_item():
-    item = request.args.get("item")
-    amount = request.args.get("amount")
     if 'user_items' not in session:
-        session['user_items'] = [{'item': item, 'amount':amount}]
+        try:
+            session['user_items'] = {request.args.get("item"): int(request.args.get("amount"))}
+        except:
+            session['user_items'] = {request.args.get("item"): 1}
     else:
-        session['user_items'].append({'item': item, 'amount':amount})
+        try:
+            if request.args.get("item") not in session['user_items']:
+                session['user_items'][request.args.get("item")] = int(request.args.get("amount"))
+            else:
+                session['user_items'][request.args.get("item")] += int(request.args.get("amount"))
+        except:
+            if request.args.get("item") not in session['user_items']:
+                session['user_items'][request.args.get("item")] = 1
+            else:
+                session['user_items'][request.args.get("item")] += 1
+
+    update_ingredients()
+    response = {"items": [], "ingredients": []}
+    for i in session['user_items'].keys():
+        response["items"].append({"data": getRecipe("recipes/" + i), "amount": session['user_items'][i]})
+    for i in session['ingredients'].keys():
+        response["ingredients"].append({"data": getRecipe("recipes/" + i), "amount": session['ingredients'][i]})
+    return json.dumps(response)
+
+@app.route('/delete_all', methods=['GET', 'POST'])
+def delete_all_items():
+    session['user_items'] = {}
+    session['ingredients'] = {}
+    return Response(status=204)
+
+def getRecipe(path):
+    with open(path) as json_file:
+        data = json.load(json_file)
+        title = " ".join([x.capitalize() for x in path[path.rfind("/") + 1:].replace(".json", "").split("_")])
+        group = data['group'] if 'group' in data else "None"
+        type = data['type'] if 'type' in data else "None"
+
+        icon_path = "icons/" + path[path.rfind("/") + 1:].replace(".json", ".png")
+        icon = icon_path if os.path.exists(icon_path) else ""
+
+        key = data['key'] if 'key' in data else {}
+        result = data['result'] if 'result' in data else {}
+        pattern = data['pattern'] if 'pattern' in data else []
+        ingredients = data['ingredients'] if 'ingredients' in data else (
+            data['ingredient'] if 'ingredient' in data else "")
+        count = data['count'] if 'count' in data else -1
+        return {
+            "title": title,
+            "path": path,
+            "pattern": pattern,
+            "key": key,
+            "result": result,
+            "group": group,
+            "type": type,
+            "ingredients": ingredients,
+            "count": count,
+            "icon": icon,
+        }
+
+
+def update_ingredients():
+    session['ingredients'] = {}
+    if 'user_items' not in session:
+        session['user_items'] = {}
+    else:
+        session['ingredients'] = {}
+        for item in session['user_items'].keys():
+            if os.path.exists("recipes/" + item):
+                data = getRecipe('recipes/' + item)
+                if data["ingredients"] != "":
+                    for ingredient in data["ingredients"]:
+                        if itemToRecipePath(ingredient['item']) not in session['ingredients']:
+                            session['ingredients'][itemToRecipePath(ingredient['item'])] = 1
+                        else:
+                            session['ingredients'][itemToRecipePath(ingredient['item'])] += 1
+                elif data["key"]:
+                    keys = {}
+                    for key in data["key"].keys():
+                        keys[key] = 0
+                    for row in data["pattern"]:
+                        for character in row:
+                            if character in keys:
+                                keys[character] += 1
+
+                    for key in keys.keys():
+                        print(itemToRecipePath(data["key"][key]["item"]))
+                        if itemToRecipePath(data["key"][key]["item"]) not in session['ingredients']:
+                            session['ingredients'][itemToRecipePath(data["key"][key]["item"])] = keys[key] * session['user_items'][item]
+                        else:
+                            session['ingredients'][itemToRecipePath(data["key"][key]["item"])] += keys[key] * session['user_items'][item]
+                    print(session['ingredients'])
 
 def search(term):
     output = []
